610e44274fb25c7a0482fa76298bffee203336c2
Pham Chi Hieu <163157290+hieupham03@users.noreply.github.com>
2026-05-30 12:16:37 +0700

    Update bit_math.h

--- L0_C_Foundation/bit_math.h
+++ L0_C_Foundation/bit_math.h
@@ -1,21 +1,16 @@
 #ifndef BIT_MATH_H
 #define BIT_MATH_H
 
-/*
- * Các macro thao tác bit cơ bản
- * Dùng toán tử bitwise để tác động vào thanh ghi (hoặc biến)
- */
-
 // Set bit lên 1 tại vị trí BIT
 #define SET_BIT(REG, BIT)    ((REG) |= (1U << (BIT)))
 
 // Clear bit về 0 tại vị trí BIT
 #define CLEAR_BIT(REG, BIT)  ((REG) &= ~(1U << (BIT)))
 
-// Lật trạng thái bit (1->0, 0->1) tại vị trí BIT
+// Lật trạng thái bit
 #define TOGGLE_BIT(REG, BIT) ((REG) ^= (1U << (BIT)))
 
-// Đọc bit tại vị trí BIT (kết quả trả về là khác 0 nếu bit = 1, bằng 0 nếu bit = 0)
+// Đọc bit tại vị trí BIT
 #define READ_BIT(REG, BIT)   (((REG) >> (BIT)) & 1U)
 
-#endif // BIT_MATH_H
+#endif 
